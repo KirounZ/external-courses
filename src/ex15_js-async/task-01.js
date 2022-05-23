2b6c02c3ea31e602462335cd0f8e96c784a7f61f
Козочкин Дмитрий <dima-kozochkin@mail.ru>
2022-05-23 21:31:52 +0300

    fix

--- src/ex15_js-async/task-01.js
+++ src/ex15_js-async/task-01.js
@@ -1,7 +1,7 @@
-function Get(jsonFile, GetOrSet) {
+function Get(jsonFile) {
   const xhr = new XMLHttpRequest();
 
-  xhr.open(String(GetOrSet), String(jsonFile), true);
+  xhr.open('Get', String(jsonFile), true);
 
   xhr.send();
 
@@ -16,4 +16,23 @@ function Get(jsonFile, GetOrSet) {
   };
 }
 
-Get('task-02.js', 'GET');
+function Post(jsonFile) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.open('Post', String(jsonFile), true);
+
+  xhr.send();
+
+  xhr.onreadystatechange = function processing() {
+    if (xhr.readyState !== 4) return;
+
+    if (xhr.status !== 200) {
+      console.log(`${xhr.status}:${xhr.statusText}`);
+    } else {
+      console.log(xhr.responseText);
+    }
+  };
+}
+
+Get('task-02.js');
+Post('task-02.js');
